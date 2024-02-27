2597a90b698bf2e9f8bb27cb613194109324d855
Vivek Chaturvedi <109510513+vivekchaturvedi1999@users.noreply.github.com>
2024-02-27 17:21:57 +0530

    Add files via upload

---
+++ jarvis.py
@@ -0,0 +1,82 @@
+import speech_recognition as sr
+import pyttsx3
+import datetime
+import wikipedia
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+
+engine.setProperty('voice', voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishme():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+    elif hour>=12 and hour<18:
+        speak("Good Afernoon!")
+    else:
+        speak("Good Evening!")
+
+    speak("I am jarvis sir please tell me how may i help you help")
+
+
+def takeCommand():
+    # It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in') #Using google for voice recognition.
+        print(f"User said: {query}\n") #User query will be printed.
+
+    except Exception as e:
+        # print(e)
+        print("Say that again please...")  #Say that again will be printed in case of improper voice
+        return "None"
+    return query
+
+
+if __name__=="__main__":
+    wishme()
+    while True:
+        query = takeCommand().lower()
+        if 'wikipedia' in query:   #if wikipedia found in the query then this block will be executed
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia","")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+        elif 'play music' in query:
+            music_dir = 'C:\\Users\\DELL\\Downloads\\music'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[3]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\DELL\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+
+
